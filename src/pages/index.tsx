260e6a12ca9e96b3d51396052755af6ffaff08f0
Abraham Cuenca <websiteprojectmanager@gmail.com>
2023-02-24 22:33:18 -0800

    Remove gatsby branding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,23 @@ import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import { Container, Row, Col, Button } from 'reactstrap';
 
+import photo from '../images/abe.jpg';
+
 const IndexPage: React.FC<PageProps> = () => {
   return (
       <Container>
           <Row>
               <Col>
-                  <p>Hello, World!</p>
+                  <h1>Hello, World!</h1>
+              </Col>
+          </Row>
+          <Row>
+              <Col>
+                  <img src={photo} className="img-fluid img-thumbnail rounded-5"/>
+              </Col>
+          </Row>
+          <Row className="mt-1 mb-1_5">
+              <Col>
                   <Button color="primary">This is a button</Button>
               </Col>
           </Row>
